4a53e3259f4df94646993325c099c445d8f84810
文坦 <wentan.lwt@alibaba-inc.com>
2026-04-17 20:19:11 +0800

    chore: restore MAX_BUCKET_SIZE_KB after bucket-full test passed

--- functions/lib/constants.js
+++ functions/lib/constants.js
@@ -2,7 +2,7 @@
 
 export const PART_SIZE = 20 * 1024 * 1024; // 20MB per part
 export const MAX_FILE_SIZE = 5 * 1024 * 1024 * 1024; // 5GB max file size
-export const MAX_BUCKET_SIZE_KB = 1; // TEMP: 1KB for bucket-full testing (restore to 5 * 1024 * 1024 after test)
+export const MAX_BUCKET_SIZE_KB = 5 * 1024 * 1024; // 5GB in KB (GitHub API returns size in KB)
 export const BUCKET_PREFIX = "lwtstore-bucket-";
 export const GITHUB_API = "https://api.github.com";
 export const RAW_GITHUB = "https://raw.githubusercontent.com";
