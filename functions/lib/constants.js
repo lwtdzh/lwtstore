1d4b001246a91e55cccae8ac1477e369c6a6e962
文坦 <wentan.lwt@alibaba-inc.com>
2026-04-17 17:53:29 +0800

    chore: change GITHUB_OWNER to lwtdzh

--- functions/lib/constants.js
+++ functions/lib/constants.js
@@ -3,7 +3,7 @@
 export const PART_SIZE = 20 * 1024 * 1024; // 20MB per part
 export const MAX_FILE_SIZE = 5 * 1024 * 1024 * 1024; // 5GB max file size
 export const MAX_BUCKET_SIZE_KB = 5 * 1024 * 1024; // 5GB in KB (GitHub API returns size in KB)
-export const GITHUB_OWNER = "lwtstore-auto-operator";
+export const GITHUB_OWNER = "lwtdzh";
 export const BUCKET_PREFIX = "lwtsub-bucket-";
 export const GITHUB_API = "https://api.github.com";
 export const RAW_GITHUB = "https://raw.githubusercontent.com";
